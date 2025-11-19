5e1745bf2ef64719351e13c40ac5e8169bfa15dd
David Ticona Saravia <david.ticona.saravia@gmail.com>
2025-11-18 23:38:27 -0400

    agrega función para obtener el listado de categorias

--- src/composables/quizes.js
+++ src/composables/quizes.js
@@ -4,6 +4,7 @@ import { shuffleArray } from '@/utils/utils';
 
 export function useQuizes() {
   const isLoading = ref(false);
+
   const getQuizData = async (quizName) => {
     isLoading.value = true;
     try {
@@ -16,8 +17,21 @@ export function useQuizes() {
     }
   };
 
+  const getQuizCategories = async () => {
+    isLoading.value = true;
+    try {
+      const { data } = await httpClient.get('data/categories.json');
+      return data;
+    } catch (err) {
+      throw new Error(err.message);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
   return {
     isLoading,
     getQuizData,
+    getQuizCategories
   };
 }
